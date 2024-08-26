a9fd9bdfb83539684b28aa52af557835972552bb
NetCreature <agustinmediotti@gmail.com>
2024-08-26 13:53:50 -0300

    #taller-de-progra: p1,e2

--- taller-de-progra/modulo_imperativo/practica_1/Ejercicio2.pas
+++ taller-de-progra/modulo_imperativo/practica_1/Ejercicio2.pas
@@ -1,7 +1,11 @@
-program ejercicio2;
+program Ejercicio_2;
+
+const 
+  FIN = -1;
+  DIMF = 300;
 
 type
-  rango_oficinas = 1..300;
+  rango_oficinas = 1..DIMF;
   oficina = record
     cod: integer;
     dni: integer;
@@ -9,33 +13,56 @@ type
   end;
   vector = array[rango_oficinas] of oficina;
 
-procedure OrdenarVector(var vec; dimL:integer);
-var i,j,pos,item:integer;
+procedure LeerOficina(var ofi:oficina);
 begin
- for i:=1 to dimL-1 do begin
-  pos:=i;
-  for j:=i+1 to dimL do
-    if vec[j] < vec[pos] then pos:=j;
-
-  item:=vec[pos];
-  vec[pos]:=vec[i];
-  vec[i]:=item;
- end;
+  readln(ofi.cod);
+  ofi.dni:= random(150);
+  ofi.val_exp:= random(20);
 end;
 
-procedure LeerOficina(var oficina);
+procedure OrdenarSeleccion(var vec:vector; dimL:integer);
+var
+  x,i,aux:integer;
+  item:oficina;
 begin
-  readln(oficina.cod);
-  oficina.dni:= random(150);
-  oficina.val_exp:= random(20);
+  for x:=1 to dimL-1 do 
+  begin
+    aux:=x;
+    for i:=x+1 to dimL do
+      begin
+        if vec[i].cod < vec[aux].cod then aux:=i;
+      end;
+  end;
+
+  item:=vec[aux];
+  vec[aux]:=vec[x];
+  vec[x]:=item;
+end;
+
+procedure OrdenarInsercion(var vec: vector; dimL: integer);
+var
+  i,j:integer;
+  act:oficina;
+begin
+  for i:=2 to dimL do
+  begin
+    act:=vec[i];
+    j:=i-1;
+    while(j>0) and (vec[j].cod > act.cod) do
+      begin
+        vec[j+1]:=vec[j];
+        j:=j-1;
+      end;
+      vec[j+1]:= act;
+  end;
 end;
 
 procedure CargarVector(var vec:vector; var dimL: integer);
 var ofi:oficina;
 begin
   LeerOficina(ofi);
-  while ofi.cod <> -1 do begin
-    vec[dimL] = ofi;
+  while ((ofi.cod <> FIN) and (dimL < DIMF)) do begin
+    vec[dimL]:= ofi;
     LeerOficina(ofi);
     dimL:=dimL+1;
   end;
@@ -45,6 +72,7 @@ var
   vec_ofi: vector; dimL: integer;
 begin
   dimL:=0;
-  CargarVector(vec_ofi, dimL);
-  OrdenarVector(vec_ofi, dimL);
+  CargarVector(vec_ofi, dimL);      {inciso a}
+  OrdenarSeleccion(vec_ofi, dimL);  {inciso b}
+  OrdenarInsercion(vec_ofi, dimL);  {inciso c}
 end.
