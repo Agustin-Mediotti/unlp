a81daf8d97bf38657217e70be2c272d612070d48
NetCreature <agustinmediotti@gmail.com>
2024-09-19 22:25:43 -0300

    #taller-de-progra: p1,e3

--- taller-de-progra/modulo_objetos/practica_1/TP_ProyectoAlumnos/src/tema1/Ej02Jugadores.java
+++ taller-de-progra/modulo_objetos/practica_1/TP_ProyectoAlumnos/src/tema1/Ej02Jugadores.java
@@ -2,25 +2,33 @@
 package tema1;
 
 //Paso 1: Importar la funcionalidad para lectura de datos
-
+import PaqueteLectura.Lector;
 
 public class Ej02Jugadores {
 
   
     public static void main(String[] args) {
         //Paso 2: Declarar la variable vector de double 
-        
+        double [] vec;
         //Paso 3: Crear el vector para 15 double 
-        
+        vec = new double[15];
         //Paso 4: Declarar indice y variables auxiliares a usar
-                
+         int i, cant = 0;
+         double aux = 0;
         //Paso 6: Ingresar 15 numeros (altura), cargarlos en el vector, ir calculando la suma de alturas
-        
+        for (i = 0; i < 15; i++) {
+            vec[i] = Lector.leerDouble();
+            aux = aux + vec[i];
+        }
         //Paso 7: Calcular el promedio de alturas, informarlo
-        
+        System.out.println("Promedio de alturas: " + aux/15);
         //Paso 8: Recorrer el vector calculando lo pedido (cant. alturas que están por encima del promedio)
-     
+        for (i = 0; i < 15; i++) {
+            if (vec[i] > aux/15)
+                cant++;
+        }
         //Paso 9: Informar la cantidad.
+        System.out.println("Cant. alturas por encima del promedio: " + cant);
     }
     
 }
--- taller-de-progra/modulo_objetos/practica_1/TP_ProyectoAlumnos/src/tema1/Ej03Matrices.java
+++ taller-de-progra/modulo_objetos/practica_1/TP_ProyectoAlumnos/src/tema1/Ej03Matrices.java
@@ -6,23 +6,60 @@
 package tema1;
 
 //Paso 1. importar la funcionalidad para generar datos aleatorios
+import PaqueteLectura.GeneradorAleatorio;
+
 
 public class Ej03Matrices {
 
     public static void main(String[] args) {
-	    //Paso 2. iniciar el generador aleatorio     
-	 
+	//Paso 2. iniciar el generador aleatorio     
+	 GeneradorAleatorio.iniciar();
         //Paso 3. definir la matriz de enteros de 5x5 e iniciarla con nros. aleatorios 
-    
+         int[][] matrix  = new int [5] [5];
+         int i,j;
+         for (i = 0; i < 5; i++) {
+             for (j = 0; j < 5; j++) {
+                 matrix[i][j] = GeneradorAleatorio.generarInt(200);
+             }
+         }
         //Paso 4. mostrar el contenido de la matriz en consola
-    
+         for (i = 0; i < 5; i++) {
+             System.out.println();
+             System.out.println("--------------------------");
+             for (j = 0; j < 5; j++) {
+                 System.out.print(matrix[i][j] + " | ");
+             }
+             System.out.println();
+             System.out.println("--------------------------");
+         }
         //Paso 5. calcular e informar la suma de los elementos de la fila 1
+         System.out.println();
+         
+         int sumFila = 0;
+         for (i = 0; i< 5; i++) sumFila+= matrix[1][i];
+         
+         System.out.println("Suma de los elementos de la fila 1: " + sumFila);
     
         //Paso 6. generar un vector de 5 posiciones donde cada posición j contiene la suma de los elementos de la columna j de la matriz. 
         //        Luego, imprima el vector.
+         int [] vec = new int [5];
+         int sum = 0;
+         for (i=0; i<5;i++) {
+             for(j=0; j<5;j++) {
+                sum += matrix[i][j];
+                vec[j] = sum;
+             }
+         }
+         
+         for (i=0; i<5; i++) System.out.println(vec[i]);
 
         //Paso 7. lea un valor entero e indique si se encuentra o no en la matriz. En caso de encontrarse indique su ubicación (fila y columna)
         //   y en caso contrario imprima "No se encontró el elemento".
-
+        int dato = PaqueteLectura.Lector.leerInt();
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 5; j++) {
+                if (matrix[i][j] == dato) System.out.println("Numero encontrado en posicion: Fila: ["+ i +"]"+" Columna: ["+ j +"]");
+            }
+        }
     }
 }
