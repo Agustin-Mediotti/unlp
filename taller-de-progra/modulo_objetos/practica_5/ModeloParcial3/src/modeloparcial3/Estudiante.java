85482704bbba2a41e1af4c115d2e39d039493f98
NetCreature <agustinmediotti@gmail.com>
2024-11-11 17:44:34 -0300

    #taller-de-progra: p5,e3

---
+++ taller-de-progra/modulo_objetos/practica_5/ModeloParcial3/src/modeloparcial3/Estudiante.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2024 netcreature.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package modeloparcial3;
+
+/**
+ *
+ * @author netcreature
+ */
+public class Estudiante {
+    private String nombre;
+    private String apellido;
+    private int dni;
+
+    public Estudiante(String nombre, String apellido, int dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+
+}
