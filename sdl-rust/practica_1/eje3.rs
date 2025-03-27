f8fffa31e185e53afba61d64e18fa576ec082559
Vitto A. Mediotti <agustinmediotti@gmail.com>
2025-03-26 22:40:25 -0300

    #sdl-rust: p1,e3

---
+++ sdl-rust/practica_1/eje3.rs
@@ -0,0 +1,23 @@
+use std::io;
+
+fn main() {
+    let boolean: bool = false;
+    let mut input = String::new();
+
+    println!("enter true or false: ");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("failed to read line!");
+
+    match input.trim() {
+        "true" | "t" => {
+            println!("{boolean} & true = {}", boolean & true);
+            println!("{boolean} | true = {}", boolean | true);
+        },
+        "false" | "f" => {
+            println!("{boolean} & false = {}", boolean & false);
+            println!("{boolean} | false = {}", boolean | false);
+        },
+        _ => println!("{} is not a boolean", input.trim()),
+    }
+}
