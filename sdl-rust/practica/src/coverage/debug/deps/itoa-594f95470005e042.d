07968b50dc2de183b3eb2f0cc5ee71c93ce5ee4d
Vitto A. Mediotti <agustinmediotti@gmail.com>
2025-07-13 19:03:27 -0300

    #folder-strcuture: update .gitignore

--- sdl-rust/practica/src/coverage/debug/deps/itoa-594f95470005e042.d
+++
@@ -1,8 +0,0 @@
-/home/netcreature/dev/unlp/sdl-rust/practica/src/coverage/debug/deps/libitoa-594f95470005e042.rmeta: /home/netcreature/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs /home/netcreature/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs
-
-/home/netcreature/dev/unlp/sdl-rust/practica/src/coverage/debug/deps/libitoa-594f95470005e042.rlib: /home/netcreature/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs /home/netcreature/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs
-
-/home/netcreature/dev/unlp/sdl-rust/practica/src/coverage/debug/deps/itoa-594f95470005e042.d: /home/netcreature/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs /home/netcreature/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs
-
-/home/netcreature/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs:
-/home/netcreature/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs:
