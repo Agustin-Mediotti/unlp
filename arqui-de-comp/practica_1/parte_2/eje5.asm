564b9584117076fe8c6c0702b155f2a2b7e5f006
NetCreature <agustinmediotti@gmail.com>
2025-09-03 17:58:40 -0300

    #arqui-de-comp: p1,pt2:e5

--- arqui-de-comp/practica_1/parte_2/eje5.asm
+++ arqui-de-comp/practica_1/parte_2/eje5.asm
@@ -1,89 +1,43 @@
-ORG 527H
-PSW     DB "H4CK"
-SUCCESS DB "Acceso Permitido"  ; 15b
-DENIED  DB "Acceso Denegado"   ; 14b
+; Acceso con contraseña con intentos limitados
+; Autor: Agustin Mediotti
+; Fecha: 03-09-2025
 
-ORG 652H
-TRIES   DB 0
-INPUT   DB ?
+ORG 1000h
+      TRIES    db  5
+      MSJ_LOG  db  "Por favor, ingrese la contraseña"
+      SUCCESS  db  0Ch, "Acceso Permitido"
+      DENIED   db  0Ch, "Acceso Denegado"
+      FAILED   db  0Ch, "Acceso Bloqueado"
+      PSW      db  "H4CK"
+      INPUT    db  ?
 
-ORG 2670
-;  Subrutina READ
-;  Entrada:
-;    bx: dir de lectura
-;    al: cantidad de caracteres a leer
-;    ah: intentos de lectura
-ERR:   INC TRIES       
-       CMP TRIES, 5       ; CHECK TRIES
-       JZ DEN
-       MOV AH, 0
-       MOV BX, OFFSET INPUT
-READ:  INT 6
-       INC BX
-       INC AH
-       CMP AH, 4
-       JNZ READ
-       
-    ;  CHECK PASS
-
-       MOV BX, OFFSET PSW     ; <- BX es OFFSET PSW
-       MOV DX, OFFSET INPUT   ; <- DX es OFFSET INPUT
-NEXTC: MOV AL, [BX]           ; <- AL es PSW 
-       CALL SWAP              ;  BX = INPUT, DX = PSW
-       MOV AH, [BX]           ; <- AH es INPUT
-
-       CMP AL, AH             ; Comparo el caracter
-       JNZ ERR
-
-       CALL SWAP              ;  BX = PSW, DX = INPUT
-       MOV CX, OFFSET PSW + 3
-       CMP BX, CX             ; ULTIMO CARACTER?
-       JZ SUC
-       INC BX
-       INC DX
-       JMP NEXTC
-       
-       RET
-
-ORG 3500
-;Subrutina Success
-SUC:   MOV BX, OFFSET SUCCESS
-       MOV AL, 1
-       MOV CX, OFFSET SUCCESS + 15
-       CALL PRINT
-       RET
-
-ORG 3600
-;Subrutina Denied
-DEN:   MOV BX, OFFSET DENIED
-       MOV AL, 1
-       MOV CX, OFFSET DENIED + 14
-       CALL PRINT
-       RET
-
-ORG 4000
-; Imprimir en pantalla
-PRINT: INT 7
-       CMP CX, BX       
-       JZ FIN
-       INC BX
-       JMP PRINT
-FIN:   RET
-
-ORG 1290H
-; Subrutina SWAP
-; Intercambia los valores de dos registros
-SWAP:  PUSH BX
-       PUSH DX
-       POP BX
-       POP DX
-       RET
-; REGISTER SWAP BABY!
-
-ORG 2000H
-       MOV BX, OFFSET INPUT
-       MOV AL, 1
-       MOV AH, 0  ;  cant de caracteres de lectura
-       CALL READ
-       INT 0
+ORG 2000h
+      MOV bx, offset MSJ_LOG
+      MOV al, offset SUCCESS - offset MSJ_LOG
+TRY:  INT 7
+      MOV al, 4
+      MOV bx, offset INPUT
+LOOP: INT 6
+      INC bx
+      DEC al
+      JNZ LOOP
+      MOV bx, offset PSW
+      MOV cl, 4
+NEXT: MOV al, [BX]
+      CMP al, [BX + 4]
+      JNZ ERR
+      INC bx
+      DEC cl
+      JNZ NEXT
+      MOV bx, offset SUCCESS
+      MOV al, offset DENIED - offset SUCCESS
+      JMP FIN
+ERR:  MOV bx, offset DENIED
+      MOV al, offset FAILED - offset DENIED
+      DEC TRIES
+      JNZ TRY
+      MOV bx, offset FAILED
+      MOV al, offset PSW - offset FAILED
+FIN:  INT 7
+      INT 0
 END
