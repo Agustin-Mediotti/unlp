c62a4761dd2e817ea832d2e0792712cb63268b2b
NetCreature <agustinmediotti@gmail.com>
2025-09-03 16:59:05 -0300

    #arqui-de-comp: p1,pt2:e4

--- arqui-de-comp/practica_1/parte_2/eje4_a.asm
+++ arqui-de-comp/practica_1/parte_2/eje4_a.asm
@@ -1,22 +1,18 @@
-ORG 2750H
-;  Subrutina print
-;  Entrada:
-;    bx: dir de los char
-;    al: cant de caracteres a imprimir
-print: INT 7
-       CMP BYTE PTR [bx], 5Ah
-       JZ fin
-       INC A
-       JMP print
-fin:   RET
+; Mostrar letras 'A' a 'Z' en pantalla
+; Autor: Agustin Mediotti
+; Fecha: 03-09-2025
 
-ORG 500H
-;  Caracter letra 'A' + salto de linea
-A DB 41h, 0AH
+ORG 1000h
+      ;  Caracter letra 'A' + salto de linea
+      A db 41h, 0Ah
 
 ORG 2000H
       MOV bx, offset A
       MOV al, 2
-      CALL print
+      MOV cl, 26
+PRNT: INT 7
+      INC A
+      DEC cl
+      JNZ PRNT
       INT 0
 END
