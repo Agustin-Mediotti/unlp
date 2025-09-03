f08cb69a6e39d233b5049616ad9b342f088a0743
NetCreature <agustinmediotti@gmail.com>
2025-09-03 15:55:10 -0300

    #arqui-de-comp: p1,e4

--- arqui-de-comp/practica_1/parte_1/eje4.asm
+++ arqui-de-comp/practica_1/parte_1/eje4.asm
@@ -1,37 +1,17 @@
-ORG 500h
-        mensaje db "Hola Mundo"
-        fin db 0
+; Programa: Convierte string a minusculas
+; Autor: Agustin Mediotti
+; Fecha: 03-09-2025
 
-ORG 3000h
-;  Subrutina a_minus
-; ENTRADA:
-;  cl, caracter ASCII en nayusculas
-; SALIDA:
-;  cl: caracter ASCII en minuscula
-a_minus:
-      OR cl, 00100000b  ; mascara 20h
-      RET
-
-ORG 2500h
-;  Subrutina iter_str
-; ENTRADA:
-;  bx, dir del mensaje
-;  dx, dir de la ultima posicion del mensaje en memoria
-iter_str:
-      MOV cl, [bx]
-      CMP cl, 61h   ; minuscula?
-      JNC sig
-      CALL a_minus
-      MOV [bx], cl 
-
-sig:  ADD bx, 1     ; siguiente
-      CMP bx, dx
-      JNZ iter_str  ; ultimo?
-      RET
+ORG 1000h
+      MENSAJE  db  "Hola, Buenas Tardes"
+      FIN      db  ?
 
 ORG 2000h
-        MOV bx, offset mensaje
-        MOV dx, offset fin
-        CALL iter_str
-        int 0
+      MOV bx, offset MENSAJE
+      MOV al, offset FIN - offset MENSAJE
+NEXT: OR byte ptr [bx], 00100000b
+      INC bx
+      DEC al
+      JNZ NEXT
+      INT 0
 END
