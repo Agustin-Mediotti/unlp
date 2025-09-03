c1e3d5e500b93c5170a381c383ea8fa18925b904
NetCreature <agustinmediotti@gmail.com>
2025-09-03 15:05:17 -0300

    #arqui-de-comp: p1,e1

--- arqui-de-comp/practica_1/parte_1/eje1.asm
+++ arqui-de-comp/practica_1/parte_1/eje1.asm
@@ -1,29 +1,19 @@
-ORG 500h
-           str db "Arquitectura de Computadoras"
-           cant db 0
+; Programa que cuenta la cantidad de letras 'a' en una cadena
+; Autor: Agustin Mediotti
+; Fecha: 03-09-2025
 
-ORG 3000h
-;  Subrutina iter_str
-;  ENTRADA:
-;    bx: dir del mensaje
-;    ax: dir de var cant
-;    cl: cant
-;  SALIDA:
-;    cl: cant
-iter_str:  MOV dl, [bx]
-           CMP dl, 61h   ; ASCII 'a'?
-           JNZ sig
-           ADD cl, 1     ; cant++
-sig:       ADD bx, 1
-           CMP ax, bx    ; ultimo caracter?
-           JNZ iter_str
-           RET
+ORG 1000h
+        str     db "Arquitectura de Computadoras"
+        cant    db 0
 
 ORG 2000h
-           MOV bx, offset str
-           MOV ax, offset cant
-           MOV cl, cant
-           CALL iter_str
-           MOV cant, cl
-           int 0
+        MOV bx, offset str
+        MOV AL, offset cant - offset str
+LOOP:   CMP byte ptr [BX], 'a'
+        JNZ NEXT
+        INC cant
+NEXT:   INC bx
+        DEC al
+        JNZ LOOP
+        INT 0
 END
