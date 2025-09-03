68978d4f7ea952f77ddd1b588c2321a3424fd1bf
NetCreature <agustinmediotti@gmail.com>
2025-09-03 15:26:40 -0300

    #arqui-de-comp: p1,e2

--- arqui-de-comp/practica_1/parte_1/eje2.asm
+++ arqui-de-comp/practica_1/parte_1/eje2.asm
@@ -1,26 +1,22 @@
-ORG 500h
-           c db 'C'
-           res db 0
+; Programa que verifica si un caracter es una letra mayuscula
+; Si es una letra mayuscula, almacena 0FFh en RES
+; Si no, almacena 00h en RES
 
-ORG 3000h
-;  Subrutina es_mayus
-;  ENTRADA:
-;    bl: Caracter
-;    ah: Resultado de la operacion
-;  SALIDA:
-;    ah: Resultado de la operacion
-es_mayus:  CMP bl, 41h
-           JC no_es
-           CMP bl, 54h
-           JNC no_es
-           MOV bh, 0FFh
-           JMP fin
-no_es:     MOV bh, 0
-fin:       RET
+; Autor: Agustin Mediotti
+; Fecha: 03-09-2025
+
+ORG 1000h
+      C   db 'C'
+      RES db ?
+
 
 ORG 2000h
-           MOV bl, c
-           MOV bh, res
-           CALL es_mayus
-           int 0
+      MOV RES, 0
+      MOV bl, C
+      CMP bl, 41h
+      JC  FIN
+      CMP bl, 5Ah
+      JNC FIN
+      MOV RES, 0FFh
+FIN:  INT 0
 END
