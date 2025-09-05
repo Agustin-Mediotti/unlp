a8c71083748a76ff7ca0dbbb3464913d83eb28b6
NetCreature <agustinmediotti@gmail.com>
2025-09-05 01:49:57 -0300

    #arqui-de-comp: p1,pt4:e4_c

---
+++ arqui-de-comp/practica_1/parte_4/Ejercicio4_c.asm
@@ -0,0 +1,45 @@
+; Autor: Agustin Mediotti
+; Fecha: 05-09-2025
+
+ORG 1000h
+        A   db  2
+        B   db  8
+        RES dw  ?
+
+ORG 3000h
+; Multiplica dos numeros de 8 bits
+; Recibe:
+;   [SP+8]: primer numero
+;   [SP+10]: segundo numero
+; Devuelve:
+;   AX: resultado de la multiplicacion por valor
+MUL:    PUSH bx
+        PUSH dx
+        PUSH cx
+        CMP al, 0
+        JZ ZERO
+        CMP ah, 0
+        JZ ZERO
+        MOV dl, ah
+        XOR dh, dh
+        XOR ah, ah
+        XOR cx, cx
+LOOP:   ADD cx, dx
+        DEC ax
+        JNZ LOOP
+        MOV ax, cx
+        JMP BACK
+ZERO:   XOR ax, ax
+BACK:   POP cx
+        POP dx
+        POP bx
+        MOV RES, ax
+        MOV ax, offset RES
+        RET
+
+ORG 2000h
+        MOV al, A
+        MOV ah, B
+        CALL MUL
+        INT 0
+END
